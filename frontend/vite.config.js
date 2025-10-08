e6d952a8304cc14e331608009bfc8da91d3e0a45
Muhammad Baqir <mohdbaqir110@gmail.com>
2025-10-08 12:03:42 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
